276738e0f1496243061726384cc7dec6fa004d4a
Opilane TTHK <opilane@tthk.ee>
2021-11-16 16:20:38 +0200

    firstcommit

---
+++ ChuckNorrisRandomJoke/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorrisRandomJoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string filepath = @"C:\Users\opilane\samples\ChuckJokes.txt";
+             string[] jokesAboutChuck = File.ReadAllLines(filepath);
+             Random rnd = new Random();
+             int randomIndex = rnd.Next(0, jokesAboutChuck.Length);
+             Console.WriteLine(jokesAboutChuck[randomIndex]);
+            */
+        }
+        public static string GetRandomJoke()
+        {
+            string filepath = @"C:\Users\opilane\samples\ChuckJokes.txt";
+            string[] jokesAboutChuck = File.ReadAllLines(filepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, jokesAboutChuck.Length);
+
+            return jokesAboutChuck[randomIndex];
+        }
+    }
+    }
+
---
+++ VeggieAndFruit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace VeggieAndFruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruitpath = @"C: \Users\opilane\samples\fruit.txt";
+            string[] VeggieData = File.ReadAllLines(fruitpath);
+            string veggiepath = @"C: \Users\opilane\samples\veggie.txt";
+            string[] FruitData = File.ReadAllLines(veggiepath);
+            File.WriteAllLines(fruitpath, FruitData);
+            File.WriteAllLines(veggiepath, VeggieData);
+        }
+    }
+}
